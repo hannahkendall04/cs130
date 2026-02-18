f014a6a25774ac2593c7bb3ea4ffbc3534bf4776
Hannah Kendall <hbkendall@outlook.com>
2026-02-18 11:22:55 -0800

    move subtitle extraction to main extension

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "./index.html",
-        background: "./src/background.ts",
+        background: "./src/background.js",
       },
       output: {
         entryFileNames: (chunkInfo) => {
