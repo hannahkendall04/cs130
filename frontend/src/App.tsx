49ff8716204f76e8ca4b263f2eae81d2e8e2b9cd
Shriya Char <shriyachar@Shriyas-MBP.hsd1.ca.comcast.net>
2026-03-01 20:58:12 -0800

    confirmed things are working post merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -250,7 +250,7 @@ function App() {
         <button
           id="postButton"
           className="w-full rounded bg-red-500 px-4 py-2 text-white hover:bg-red-700 disabled:cursor-not-allowed disabled:opacity-50"
-          onClick={handlePost}
+          /*onClick={handlePost}*/
           disabled={!comment.trim()}
         >
           post
